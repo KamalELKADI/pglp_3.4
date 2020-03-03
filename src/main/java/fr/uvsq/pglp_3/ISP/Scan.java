9e2545eabf75ab6f1032df212ab3cdbcfaf5b3a4
uvsq21918311 <elkadi96hotmail.com>
2020-03-03 14:18:56 +0100

    Add classe simplePrinter and interface scan

---
+++ src/main/java/fr/uvsq/pglp_3/ISP/Scan.java
@@ -0,0 +1,5 @@
+package fr.uvsq.pglp_3.ISP;
+
+public interface Scan {
+	public void scan() ;
+}
---
+++ src/main/java/fr/uvsq/pglp_3/ISP/SimplePrinter.java
@@ -0,0 +1,9 @@
+package fr.uvsq.pglp_3.ISP;
+
+public class SimplePrinter implements Print{
+	//imprime
+	public void print() {
+		/*print a document */
+	}
+
+}
